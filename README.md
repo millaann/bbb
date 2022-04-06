7553ba0ea17e9707f146c68888ca5041e58f14bc
millaann <96682381+millaann@users.noreply.github.com>
2022-04-06 13:52:42 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# bbb
-ميلآن سورس
+<h1 align="center">- sᴏᴜʀᴄᴇ ᴍɪʟᴀɴ .</h1>
+
+<p align="center">
+  <strong><a href="https://t.me/SORMILAN">- ᴛᴇʟᴇɢʀᴀᴍ ᴄʜᴀɴɴᴇʟ .</a></strong> |
+  <strong><a href="https://t.me/XB0BB">- ᴄᴏᴅᴇʀ .</a></strong>
+</p>
---
+++ setup.py
@@ -0,0 +1,38 @@
+import requests,os
+
+try:
+  from config import *
+  os.system('pm2 start bot.py --name {} --interpreter python3.7 --interpreter-args -u'.format(BOT_ID))
+except Exception as e:
+  API_ID = 4139599
+  API_HASH = 'c4e7d051da03aa0f774b686ea3b993b2'
+
+  out ="""
+API_ID = 4139599
+API_HASH = 'c4e7d051da03aa0f774b686ea3b993b2'
+"""
+  def Bot(TOKEN,method,data):
+    url = "https://api.telegram.org/bot{}/{}".format(TOKEN,method)
+    post = requests.post(url,data=data)
+    return post.json()
+  ID = ""
+  go = True
+  while go:
+    token = input("input you're bot TOKEN:")
+    get = Bot(token,"getme",{})
+    if get["ok"]:
+      out = out+"\n"+"TOKEN = '{}'\nBOT_ID = TOKEN.split(':')[0]".format(token)
+      go = False
+      ID = token.split(':')[0]
+
+    else:
+      print("TOKEN is invalid, Try again")
+
+  sudo = input("input you're ID:")
+  out = out+"\n"+"SUDO = {}".format(sudo)
+
+  f = open("config.py","w+") 
+  f.write(out)
+  f.close()
+
+  os.system('pm2 start bot.py -f --name {} --interpreter python3.7 --interpreter-args -u'.format(ID))
